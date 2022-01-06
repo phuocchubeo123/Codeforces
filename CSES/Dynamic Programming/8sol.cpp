33a94c7d1801cf115791c81d36eb4d6c77ba082b
Pham Van Long Phuoc <73516006+phuocchubeo123@users.noreply.github.com>
2022-01-06 16:51:45 +0800

    sol 8 cses dp

--- CSES/Dynamic Programming/8sol.cpp
+++ CSES/Dynamic Programming/8sol.cpp
@@ -8,7 +8,7 @@ typedef long long ll;
 #define for1(i, n) for (int i = 1; i <= n; i++)
 #define pb push_back
 
-const int N = 2e5 + 4;
+const int N = 2e5 + 4, MOD = 1e9 + 7;
 int T;
 
 void prepare(){
@@ -16,7 +16,36 @@ void prepare(){
 }
 
 void solve(){
+    int n, m; cin >> n >> m;
+    vector<int> x(n + 1);
+    for1(i, n) cin >> x[i];
+    vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+    if (x[1] == 0) for1(j, m) dp[1][j] = 1;
+    else dp[1][x[1]] = 1;
+  
+    for1(i, n){
+        if (x[i] == 0){
+            for1(j, m){
+                if (j - 1 >= 1) (dp[i][j] += dp[i - 1][j - 1]) %= MOD;
+                (dp[i][j] += dp[i - 1][j]) %= MOD;
+                if (j + 1 <= m) (dp[i][j] += dp[i - 1][j + 1]) %= MOD;
+            }
+        }
+        else{
+            for1(j, m){
+                if (j != x[i]) dp[i][j] = 0;
+                else{
+                    if (j - 1 >= 1) (dp[i][j] += dp[i - 1][j - 1]) %= MOD;
+                    (dp[i][j] += dp[i - 1][j]) %= MOD;
+                    if (j + 1 <= m) (dp[i][j] += dp[i - 1][j + 1]) %= MOD;
+                }
+            }
+        }
+    }
     
+    int tot = 0;
+    for1(i, m) (tot += dp[n][i]) %= MOD;
+    cout << tot;
 }
 
 int main(){
@@ -24,7 +53,7 @@ int main(){
     cin.tie(0);
     prepare();
 
-    cin >> T;
+    // cin >> T;
     T = 1;
 
     while (T--){
