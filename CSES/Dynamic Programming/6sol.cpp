cbb8765a1f123f9c4e4b1c67bc3e2282c44bed34
Pham Van Long Phuoc <73516006+phuocchubeo123@users.noreply.github.com>
2022-01-06 11:47:13 +0800

    sol 6 cses dp

--- CSES/Dynamic Programming/6sol.cpp
+++ CSES/Dynamic Programming/6sol.cpp
@@ -8,7 +8,7 @@ typedef long long ll;
 #define for1(i, n) for (int i = 1; i <= n; i++)
 #define pb push_back
 
-const int N = 2e5 + 4;
+const int N = 2e5 + 4, MOD = 1e9 + 7;
 int T;
 
 void prepare(){
@@ -16,7 +16,29 @@ void prepare(){
 }
 
 void solve(){
-    
+    int n; cin >> n;    
+    vector<string> dp(n);
+    for0(i, n) cin >> dp[i];
+    if (dp[n - 1][n - 1] == '*'){
+        cout << -1;
+        return;
+    }
+
+    for (int i = n - 1; i >= 0; i--){
+        for (int j = n - 1; j >= 0; j--){
+            dp[i][j] = 0;
+        }
+    }
+
+    dp[n - 1][n - 1] = 1;
+
+    for (int i = n - 1; i >= 0; i--){
+        for (int j = n - 1; j >= 0; j--){
+            if (i - 1 >= 0) (dp[i - 1][j] += dp[i][j]) %= MOD;
+            if (j - 1 >= 0) (dp[i][j - 1] += dp[i][j]) %= MOD;
+        }
+    }
+    cout << dp[0][0];
 }
 
 int main(){
@@ -24,7 +46,7 @@ int main(){
     cin.tie(0);
     prepare();
 
-    cin >> T;
+    // cin >> T;
     T = 1;
 
     while (T--){
