128a0dd5494392f13425fe085f06a7cc73aee107
Pham Van Long Phuoc <73516006+phuocchubeo123@users.noreply.github.com>
2022-01-06 16:56:10 +0800

    sol 9 incomplete

--- CSES/Dynamic Programming/9sol.cpp
+++ CSES/Dynamic Programming/9sol.cpp
@@ -8,7 +8,7 @@ typedef long long ll;
 #define for1(i, n) for (int i = 1; i <= n; i++)
 #define pb push_back
 
-const int N = 2e5 + 4;
+const int N = 1e6 + 4, MOD = 1e9 + 7;
 int T;
 
 void prepare(){
@@ -16,7 +16,15 @@ void prepare(){
 }
 
 void solve(){
-    
+    int n; cin >> n;
+    vector<int> dp(n + 1);
+    int pow_2 = 1;
+    for1(i, n - 1) (pow_2 *= 2) %= MOD;
+    dp[0] = pow_2;
+    int tot = pow_2;
+    for1(i, n){
+        
+    }
 }
 
 int main(){
