63345c6273121ac53794585eeea9d21bb0933bdd
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-01-07 12:04:01 +0800

    sol 10 cses dp

--- CSES/Dynamic Programming/10sol.cpp
+++ CSES/Dynamic Programming/10sol.cpp
@@ -8,15 +8,25 @@ typedef long long ll;
 #define for1(i, n) for (int i = 1; i <= n; i++)
 #define pb push_back
 
-const int N = 2e5 + 4;
+const int N = 1e6 + 4, MOD = 1e9 + 7;
 int T;
 
 void prepare(){
-
 }
 
 void solve(){
-    
+    int n; cin >> n;
+    vector<int> a(n + 1), b(n + 1);
+    a[0] = 1; b[0] = 1;
+    int tot_a = 1, tot_b = 1;
+    for1(i, n){
+        a[i] = (tot_a + tot_b) % MOD;
+        (tot_a += a[i]) %= MOD;
+        b[i] = (a[i] + tot_b) % MOD; 
+        (tot_b *= 2) %= MOD;
+        (tot_b += b[i]) %= MOD;
+    }
+    cout << a[n] << "\n";
 }
 
 int main(){
@@ -25,7 +35,7 @@ int main(){
     prepare();
 
     cin >> T;
-    T = 1;
+    // T = 1;
 
     while (T--){
         solve();
