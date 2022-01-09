94c6ea525e0cb8ce203c3334a0f481faa3a7d79e
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-01-09 10:12:44 +0800

    update template segtree

--- Template/segmentTree.cpp
+++ Template/segmentTree.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 typedef long long ll;
+#define vt vector
 
 const int N = 1e5;
 int n;
@@ -11,47 +12,59 @@ ll a[2 * N];
 //segTree template
 int seg_sz, seg[4 * N];
 
-void buildSegTree(){
-    seg_sz = 1;
-    while (seg_sz < n) seg_sz = seg_sz * 2;
-    for (int i = n; i < seg_sz; i++) a[i] = 0;
-    for (int i = 0; i < seg_sz; i++) seg[seg_sz + i] = a[i];
-    for (int i = seg_sz - 1; i > 0; i--) seg[i] = seg[i * 2] + seg[i * 2 + 1];
-}
+struct segTree{
+    vt<int> seg;
+    int sz;
 
-void updateSegTree(int pos, ll val){
-    int ps = seg_sz + pos;
-    while (ps > 0){
-        seg[ps] += val;
-        ps = ps / 2;
+    segTree(vt<int> v, int n){
+        sz = 1;
+        while (sz < n) sz = sz * 2;
+        seg = vt<int> (2 * sz);
+        build(v, n);
     }
-}
 
-ll querySegTree(int l, int r){
-    l = seg_sz + l;
-    r = seg_sz + r;
-    ll qr = 0;
-
-    while (l <= r){
-        // cout << l << " " << r << "\n";
-        if (l & 1) qr += seg[l++];
-        if (!(r & 1)) qr += seg[r--];
-        l = l / 2;
-        r = r / 2;
+    void build(vt<int> v, int n){
+        for (int i = 0; i < sz; i++){
+            if (sz + i < n) seg[sz + i] = v[i];
+            else seg[sz + i] = 0;
+        }
+        for (int i = sz - 1; i > 0; i--) seg[i] = seg[i * 2] + seg[i * 2 + 1];
     }
-    return qr;
-}
 
+    void update(int pos, ll val){
+        int ps = sz + pos;
+        while (ps > 0){
+            seg[ps] += val;
+            ps = ps / 2;
+        }
+    }
+
+    ll query(int l, int r){
+        if (r < l) return 0;
+
+        l = sz + l;
+        r = sz + r;
+        ll qr = 0;
+
+        while (l <= r){
+            // cout << l << " " << r << "\n";
+            if (l & 1) qr += seg[l++];
+            if (!(r & 1)) qr += seg[r--];
+            l = l / 2;
+            r = r / 2;
+        }
+        return qr;
+    }
+};
 
 int main(){
     n = 4;
+    vector<int> a(4);
     a[0] = 1;
     a[1] = 2;
     a[2] = 3;
     a[3] = 4;
-    buildSegTree();
-    updateSegTree(0, 2);
-    updateSegTree(1, 2);
-    cout << querySegTree(1, 3);
+
+    segTree sega(a, 4);
     return 0;
 }
