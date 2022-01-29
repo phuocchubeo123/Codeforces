d87c57135b6090335f8ae13f8cafe62487a82e9e
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-01-29 13:43:34 +0800

    div 2 768 sol f

--- Div2/768/fsol.cpp
+++ Div2/768/fsol.cpp
@@ -16,8 +16,38 @@ void prepare(){
 
 }
 
+int gcd(int x, int y){
+    if (x > y) return gcd(y, x);
+    if (y % x == 0) return x;
+    return gcd(x, y % x);
+}
+
 void solve(){
+    int n, m; 
+    cin >> n >> m;
+    vt<int> a(n), b(m);
+    for0(i, n) cin >> a[i];
+    for0(i, m) cin >> b[i];
     
+    int g = b[0];
+    for0(i, m) g = gcd(g, b[i]);
+
+    vt<bool> prty(g);
+    for0(i, n) if (a[i] < 0) prty[i % g] = prty[i % g] ^ 1;
+
+    for0(i, n) if (a[i] < 0) a[i] = -a[i];
+    vt<int> mn(g, 1e9 + 4);
+    for0(i, n) mn[i % g] = min(mn[i % g], a[i]);
+
+    ll ans1 = 0;
+    for0(i, n) ans1 += 1ll * a[i];
+    ll ans2 = ans1;
+    for0(i, g){
+        if (prty[i]) ans1 -= 1ll * 2 * mn[i];
+        else ans2 -= 1ll * 2 * mn[i];
+    }
+
+    cout << max(ans1, ans2) << "\n";
 }
 
 int main(){
@@ -26,7 +56,7 @@ int main(){
     prepare();
 
     cin >> T;
-    T = 1;
+    // T = 1;
 
     while (T--){
         solve();
