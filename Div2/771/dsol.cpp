1f39c9a1fe90c132cf236ecbb6939ab850f90d99
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-02-22 22:30:35 +0800

    div2 771 d sol

--- Div2/771/dsol.cpp
+++ Div2/771/dsol.cpp
@@ -17,7 +17,85 @@ void prepare(){
 }
 
 void solve(){
-    
+    int n, m;
+    cin >> n >> m;
+    vt<vt<int>> a(n, vt<int> (m, 0));
+    for0(i, n){
+        for0(j, m){
+            cin >> a[i][j];
+        }
+    }
+
+    vt<vt<int>> a2 = a;
+
+    vt<vt<int>> f(n, vt<int> (m));
+    for0(i, n - 1){
+        for0(j, m - 1){
+            int c = a[i][j];
+            if (a[i + 1][j] == c && a[i][j + 1] == c && a[i + 1][j + 1] == c) f[i][j] = 1;
+            else f[i][j] = 0;
+        }
+    }
+
+    stack<pair<int, int>> s;
+    for0(i, n - 1) for0(j, m - 1) if (f[i][j] == 1) s.push({i, j});
+
+    vt<tuple<int, int, int>> ans;
+    while (!s.empty()){
+        pair<int, int> p = s.top(); s.pop();
+        int i = p.first, j = p.second;
+        int c = 0;
+        for0(x, 2) for0(y, 2) if (a[i + x][j + y] != 0) c = a[i + x][j + y];
+        ans.pb({i, j, c});
+        for0(k, 2) for0(l, 2) a[i + k][j + l] = 0;
+
+        forlr(k, -1, 2, 1){
+            forlr(l, -1, 2, 1){
+                if (i + k < 0 || i + k >= n - 1) continue;
+                if (j + l < 0 || j + l >= m - 1) continue;
+                
+                set<int> se;
+                for0(x, 2) for0(y, 2) if (a[i + k + x][j + l + y] != 0) se.insert(a[i + k + x][j + l + y]);
+                if (se.size() >= 2) continue;
+                if (f[i + k][j + l] == 1) continue;
+                f[i + k][j + l] = 1;
+                s.push({i + k, j + l});
+            }
+        }
+    }
+    // cout << "\n";
+
+    reverse(ans.begin(), ans.end());
+    vt<tuple<int, int, int>> anss;
+    vt<vt<int>> b(n, vt<int> (m, 0));
+    for (auto tp: ans){
+        int i = get<0>(tp), j = get<1>(tp), c = get<2>(tp);
+        if (c == 0) continue;
+        anss.pb({i + 1, j + 1, c});
+        b[i][j] = c;
+        b[i + 1][j] = c;
+        b[i][j + 1] = c;
+        b[i + 1][j + 1] = c;
+    }
+
+    // for0(i, n){
+    //     for0(j, m) cout << b[i][j] << " ";
+    //     cout << "\n";
+    // }
+
+    for0(i, n){
+        for0(j, m){
+            if (b[i][j] != a2[i][j]){
+                cout << -1;
+                return;
+            }
+        } 
+    }
+
+    cout << anss.size() << "\n";
+    for (auto x: anss){
+        cout << get<0>(x) << " " << get<1>(x) << " " << get<2>(x) << "\n";
+    }
 }
 
 int main(){
@@ -25,7 +103,7 @@ int main(){
     cin.tie(0);
     prepare();
 
-    cin >> T;
+    // cin >> T;
     T = 1;
 
     while (T--){
