112af79ee19f16c1ef49f44ada67debf53fdb01c
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-04-09 08:56:00 +0800

    code jam 1a

--- Template/config_gen.py
+++ Template/config_gen.py
@@ -1,3 +1,3 @@
 [CONTEST]
-folder_name = ..//ICPCTrain//31
-prob_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
+folder_name = ..//Code Jam//2022//1A
+prob_list = ['a', 'b', 'c', 'd', 'e', 'f']
