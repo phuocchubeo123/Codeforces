0fbb02eadd99d7eb87d78f55a6b711b2bdddf2d1
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-05-21 15:06:29 +0800

    d i nerc 2022

--- ICPCTrain/NERC2021/dsol.cpp
+++ ICPCTrain/NERC2021/dsol.cpp
@@ -17,7 +17,33 @@ void prepare(){
 }
 
 void solve(){
+    string s, t;
+    cin >> s >> t;
+    int szs = s.length(), szt = t.length();
+    int alp[30];
+    for0(i, 30) alp[i] = 0;
+    for0(i, szt){
+        alp[(t[i] - 'A')]++;
+    }
     
+    string x;
+    for (int i = szs - 1; i >= 0; i--){
+        if (alp[(s[i] - 'A')] > 0){
+            alp[(s[i] - 'A')]--;
+            x.pb(s[i]);
+        }
+    }
+
+    reverse(x.begin(), x.end());
+    for0(i, szt){
+        if (x[i] != t[i]){
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+    return;
 }
 
 int main(){
@@ -26,7 +52,7 @@ int main(){
     prepare();
 
     cin >> T;
-    T = 1;
+    // T = 1;
 
     while (T--){
         solve();
--- ICPCTrain/NERC2021/isol.cpp
+++ ICPCTrain/NERC2021/isol.cpp
@@ -16,8 +16,54 @@ void prepare(){
 
 }
 
+int scanXY(int x, int y){
+    cout << "SCAN " << x << " " << y << "\n";
+    cout.flush();
+    int z; cin >> z;
+    return z;
+}
+
+int dig(int x, int y){
+    cout << "DIG " << x << " " << y << "\n";
+    cout.flush();
+    int z; cin >> z;
+    return z;
+}
+
 void solve(){
-    
+    int n, m;
+    cin >> n >> m;
+    int s1 = scanXY(1, 1);
+    int s2 = scanXY(1, m);
+    int abcd = s1 + 4;
+    int ac_bd = s2 - 2 * m + 2;
+    int ac = (abcd + ac_bd) / 2;
+    int bd = abcd - ac;
+    // cout << ac << " " << bd << "\n";
+    // cout.flush();
+    int ac_div_2 = ac / 2;
+    int bd_div_2 = bd / 2;
+    int s3 = scanXY(ac_div_2, 1);
+    int s4 = scanXY(1, bd_div_2);
+    int a_c = s3 - bd + 2;
+    int b_d = s4 - ac + 2;
+
+    int a = (ac + a_c) / 2;
+    int c = ac - a;
+    int b = (bd + b_d) / 2;
+    int d = bd - b;
+
+    int q1 = dig(a, b);
+    if (q1){
+        dig(c, d);
+        return;
+    }
+
+    else{
+        dig(a, d);
+        dig(c, b);
+        return;
+    }
 }
 
 int main(){
@@ -26,7 +72,7 @@ int main(){
     prepare();
 
     cin >> T;
-    T = 1;
+    // T = 1;
 
     while (T--){
         solve();
