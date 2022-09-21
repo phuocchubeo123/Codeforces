eef8bc673043ba5e5577ced1e55618b7775ca04b
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-09-21 09:44:43 +0800

    new template

---
+++ Template/bigint.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+const int N = 2e5 + 4, MOD = 998244353;
--- Template/config_gen.py
+++ Template/config_gen.py
@@ -1,3 +1,3 @@
 [CONTEST]
-folder_name = ..//Div2//813
-prob_list = ['a', 'b', 'c', 'd', 'e1', 'e2', 'f']
+folder_name = ..//ICPCTrain//35
+prob_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
