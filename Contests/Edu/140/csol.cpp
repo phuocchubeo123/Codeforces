c2df6a1f75dfb8dbdddbc8d82d4f78ae4010de55
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2022-12-22 17:59:04 +0700

    upsolve edu bitmask E, really hard problem for me and learn something new

--- Contests/Edu/140/csol.cpp
+++ Contests/Edu/140/csol.cpp
@@ -31,21 +31,140 @@ template<typename T>
 inline T findLessPower(T base, T n){if(n==1){return 0;} T temp = log(n)/log(base); if(power(base, temp) == n){return temp-1;}else{return temp;}}
 
 const int maxn = 1e5 + 5;
-const ll MOD = 1e9 + 7; // 998244353
+const ll MOD = 998244353;
 const ll INF = 1e9;
 const char min_char = 'a';
 const double EPS = 1e-9;
 const double PI = 3.14159265358979323846;
 
-void solve(){
+struct mint{
+    ll val;
+    mint(){
+        val = 0;
+    }
+    mint(ll x){
+        val = 1ll * x;
+        if (val < 0) val = MOD - (-val) % MOD;
+        if (val >= MOD) val %= MOD;
+    }
+    bool operator==(const mint& other){
+        return val == other.val;
+    }
+    mint operator+(const mint& other){
+        return mint(val + other.val);
+    }
+    mint operator-(const mint& other){
+        return mint(val - other.val);
+    }
+    mint operator*(const mint& other){
+        return mint((val * 1ll * other.val) % MOD);
+    }
+    void operator+=(const mint& other){
+        val = (mint(val) + other).val;
+    }
+    mint pow(int y){
+        if (y == 0) return mint(1);
+        if (y == 1) return mint(val);
+        mint x = mint(val).pow(y/2);
+        if (y % 2 == 0) return x * x;
+        else return x * x * mint(val);
+    }
+    mint operator/(const mint& other){
+        return mint(val) * mint(other.val).pow(MOD - 2);
+    }
 
+    friend ostream& operator<<(ostream& os, const mint& x);
+};
+
+ostream& operator<<(ostream& cout, const mint& x){
+    cout << x.val;
+    return cout;
+}
+
+void solve(){
+    int n; cin >> n;
+    vector<vi> a(n+1, vi(n+1, 0));
+    rep(i, 1, n){
+        rep(j, i, n){
+            cin >> a[i][j];
+        }
+    }
+
+    vi adj(n+1);
+    rep(i, 1, n) adj[i] = i;
+    rep(i, 1, n){
+        int max_same = i;
+        rep(j, i, n){
+            if (a[i][j] == 1) max_same = max(max_same, j);
+        }
+        rep(j, i, max_same-1){
+            adj[j] = j+1;
+        }
+    }
+
+    vi c(n+1);
+    c[n] = 1;
+
+    per(i, n-1, 1){
+        if (adj[i] == i){
+            c[i] = c[i+1] + 1;
+            continue;
+        }
+
+        adj[i] = adj[i+1];
+        c[i] = c[i+1];
+    }
+
+    per(i, n, 1) c[i] = c[1] + 1 - c[i];
+    // rep(i, 1, n) cout << c[i] << " ";
+    // cout << "\n";
+
+    vi dif(n+1, n+1);
+    rep(i, 1, n){
+        rep(j, i, n){
+            if (a[i][j] == 2){
+                if (j <= adj[i]){
+                    cout << 0;
+                    return;
+                }
+                // cout << i << " " << c[i] << " " << j << " " << c[j] << "\n";
+                dif[c[i]] = min(dif[c[i]], c[j]);
+            }
+        }
+        rep(j, 1, i) dif[c[j]] = min(dif[c[j]], dif[c[i]]);
+    }
+
+    // rep(i, 1, c[1]) cout << dif[i] << " ";
+    // cout << "\n";
+
+    vector<vector<mint>> dp(n+1, vector<mint>(2, 0));
+    dp[c[n]][0] = mint(1); dp[c[n]][1] = mint(1);
+    per(i, c[n]-1, 1){
+        if (dif[i] == n+1){
+            dp[i][0] += dp[i+1][0] + dp[i+1][1];
+            dp[i][1] += dp[i+1][0] + dp[i+1][1];
+            continue;
+        }
+
+        rep(j, i+1, dif[i]){
+            dp[i][0] += dp[j][1];
+            dp[i][1] += dp[j][0];
+        }
+        continue;
+    }
+
+    // rep(i, 1, c[n]){
+    //     cout << i << " " << dp[i][0] << " " << dp[i][1] << "\n";
+    // }
+
+    cout << dp[1][0] + dp[1][1];
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while(T--){
         solve();
     }
--- Contests/Edu/140/esol.cpp
+++ Contests/Edu/140/esol.cpp
@@ -38,14 +38,109 @@ const double EPS = 1e-9;
 const double PI = 3.14159265358979323846;
 
 void solve(){
+    int n, m; cin >> n >> m;
+    vi c(n), x(m);
+    forn(i, n){ cin >> c[i]; c[i]--;}
+    forn(i, m){ cin >> x[i];}
 
+    if (m == 1){ cout << x[0]; return;}
+
+    int mid = m / 2;
+    vector<int> adj1(m), adj2(m), adj12(m);
+    forn(i, n-1){
+        if (c[i] < mid && c[i+1] < mid){ adj1[c[i]] |= (1 << c[i+1]); adj1[c[i+1]] |= (1 << c[i]);}
+        else if (c[i] < mid) adj12[c[i]] |= (1 << (c[i+1]-mid));
+        else if (c[i+1] < mid) adj12[c[i+1]] |= (1 << (c[i]-mid));
+        else { adj2[c[i]-mid] |= (1 << (c[i+1]-mid)); adj2[c[i+1]-mid] |= (1 << (c[i]-mid));}
+    }
+
+    // forn(i, mid){
+    //     forn(j, m-mid){
+    //         if ((adj12[i] >> j) & 1) cout << 1;
+    //         else cout << 0;
+    //     }
+    //     cout << "\n";
+    // }
+    // return;
+
+    auto cost = [&](int msk){
+        ll cst = 0;
+        forn(i, mid) if ((msk >> i) & 1) cst += x[i];
+        return cst;
+    };
+
+    auto cost2 = [&](int msk){
+        ll cst = 0;
+        forn(i, m - mid) if ((msk >> i) & 1) cst += x[mid + i];
+        return cst;
+    };
+
+    vector<ll> dp((1 << (m - mid)), 1e9);
+
+    // forn(mask, (1 << mid)){
+    //     forn(i, mid) cout << ((mask >> i) & 1);
+    //     cout << "\n";
+    //     cout << cost(mask) << "\n";
+    // }
+
+    // cout << c[0] << "\n";
+
+    forn(mask, (1 << mid)){
+        if (c[0] < mid && (((mask >> c[0]) & 1) == 0)) continue;
+        if (c[n-1] < mid && (((mask >> c[n-1]) & 1) == 0)) continue;
+        int need = 0, con = 0;
+        forn(i, mid) if (((mask >> i) & 1) == 0){
+            need |= adj1[i];
+            con |= adj12[i];
+        }
+        // forn(i, mid) cout << ((mask >> i) & 1);
+        // cout << " ";
+        // forn(i, mid) cout << (((need | mask) >> i) & 1);
+        // cout << "\n";
+        // continue;
+        if ((need | mask) > mask) continue;
+        if (c[0] >= mid) con |= (1 << (c[0] - mid));
+        if (c[n-1] >= mid) con |= (1 << (c[n-1]-mid));
+        // forn(i, mid) cout << ((mask >> i) & 1);
+        // cout << " ";
+        // forn(i, m-mid) cout << ((con >> i) & 1);
+        // cout << "\n";
+        dp[con] = min(dp[con], cost(mask));
+    }
+    // cout << "\n";
+
+    ll ans = 1e9;
+
+    forn(mask, (1 << (m - mid))){
+        forn(i, m - mid){
+            if ((mask >> i) & 1) dp[mask] = min(dp[mask], dp[mask ^ (1 << i)]);
+        }
+        int need = 0;
+        forn(i, m-mid) if (((mask >> i) & 1) == 0){
+            need |= adj2[i];
+        }
+        // forn(i, mid) cout << ((mask >> i) & 1);
+        // cout << " ";
+        // forn(i, mid) cout << (((need | mask) >> i) & 1);
+        // cout << "\n";
+        // continue;
+        if ((need | mask) > mask) continue;
+        if (c[0] >= mid && ((mask >> (c[0]-mid)) & 1) == 0) continue;
+        if (c[n-1] >= mid && ((mask >> (c[n-1]-mid)) & 1) == 0) continue;
+        // forn(i, mid) cout << ((mask >> i) & 1);
+        // cout << " ";
+        // cout << cost2(mask) << " " << dp[mask] << "\n";
+        ans = min(ans, cost2(mask) + dp[mask]);
+    }
+
+    cout << ans;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while(T--){
         solve();
     }
