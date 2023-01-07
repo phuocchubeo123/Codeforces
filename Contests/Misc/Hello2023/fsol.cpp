0fb8ac4df63b03ac8fad341f189753fa28bceedd
Pham Van Long Phuoc <phuoc002@e.ntu.edu.sg>
2023-01-07 21:28:51 +0700

    upsolve hello 2023 f

--- Contests/Misc/Hello2023/fsol.cpp
+++ Contests/Misc/Hello2023/fsol.cpp
@@ -27,21 +27,81 @@ const double PI = 3.14159265358979323846;
 
 vi a(maxn+1);
 vi p(maxn+1); 
+vi des(maxn+1);
+bool can2[32];
 vector<vi> chi(maxn+1);
-vector<vector<vi>> seq(maxn, vector<vi>(32));
-vector<vi> can(maxn, vi(32, 0)), can2(maxn, vi(32, 0));
+vector<vi> par(maxn, vi(32, 0));
+vector<vector<bool>> can(maxn, vector<bool>(32, false)); 
+
+void numDescendants(int u){
+    des[u] = 1;
+    if (chi[u].empty()) return;
+    for (int v: chi[u]){ numDescendants(v); des[u] += des[v];}
+}
 
 void dfs(int u){
-    can[u][a[u]] = 1;
-    for (int v: chi[u]){
-        dfs(v);
+    if (chi[u].empty()){ can[u][a[u]] = true; return;}
+
+    if (chi[u].size() == 1){
+        int v = chi[u][0]; dfs(v);
         forn(i, 32){
+            if (can[v][i]){ can[u][i^a[u]] = true; par[v][i^a[u]] = i;}
+        }
+        if ((des[u] & 1) ^ 1) can[u][0] = true;
+        return;
+    }
+
+    for (int v: chi[u]) dfs(v);
+
+    forn(id, chi[u].size() - 1){
+        int v = chi[u][id];
+        int w = chi[u][id+1];
+        memset(can2, false, 32);
+        forn(i, 32){
+            if (!can[v][i]) continue;
             forn(j, 32){
-                can2[u][i^j] |= (can[u][i] & can[v][j]);
+                if (can[w][j]){ can2[i^j] = true; par[v][i^j] = i;}
             }
         }
-        forn(i, 32) can[u][i] = can2[u][i];
+        forn(i, 32) can[w][i] = can2[i];
     }
+
+    int v = chi[u][chi[u].size()-1];
+    memset(can2, false, 32);
+    forn(i, 32){
+        if (can[v][i]){ can2[i^a[u]] = true; par[v][i^a[u]] = i;}
+    }
+    forn(i, 32) can[u][i] = can2[i];
+    if ((des[u]&1)^1) can[u][0] = true;
+}
+
+vi steps;
+
+void backtrack(int u, int x){
+    // cout << u << " " << x << "\n";
+    if (x == 0){
+        steps.push_back(u);
+        steps.push_back(u);
+        if ((des[u] & 1) ^ 1) return;
+    }
+
+    if (chi[u].empty()) return;
+
+    if (chi[u].size() == 1){
+        int v = chi[u][0];
+        backtrack(v, par[v][x]);
+        return;
+    }
+
+    int curr = x ^ a[u];
+    for (int i = chi[u].size() - 1; i > 0; i--){
+        int v = chi[u][i-1];
+        int w = chi[u][i];
+        // cout << v << " " << w << " " << curr << "\n";
+        backtrack(w, curr ^ par[v][curr]);
+        curr = par[v][curr];
+    }
+    backtrack(chi[u][0], curr);
 }
 
 void solve(){
@@ -49,6 +109,33 @@ void solve(){
     rep(i, 1, n) cin >> a[i];
     rep(i, 2, n) cin >> p[i];
     rep(i, 2, n) chi[p[i]].push_back(i);
+
+    numDescendants(1);
+    dfs(1);
+    // forn(i, 32) cout << can[2][i] << " ";
+    // cout << "\n";
+    // forn(i, 32) cout << can[1][i] << " ";
+    // cout << "\n";
+    // rep(i, 2, n){
+    //     cout << i << " " << des[i] << " " << ((des[i] & 2) ^ 1) << "\n";
+    //     forn(j, 33) cout << can[i][j] << " ";
+    //     cout << "\n";
+    //     forn(j, 33) cout << par[i][j] << " ";
+    //     cout << "\n";
+    // }
+
+    if (!can[1][0]){ cout << -1 << "\n"; return;}
+    backtrack(1, 0);
+    reverse(all(steps));
+    // if (steps.back() != 1){
+    //     steps.push_back(1);
+    //     steps.push_back(1);
+    // }
+    cout << steps.size() << "\n";
+    for (int x: steps){
+        cout << x << " ";
+    }
+    cout << "\n";
 }
 
 int main(){
