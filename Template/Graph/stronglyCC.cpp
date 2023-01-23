aa71cdcc3efd937b101651ff174e19df2265c042
phuocchubeo123 <phuoc002@e.ntu.edu.sg>
2023-01-23 12:06:42 +0800

    strongly connected component and topo sort template update

---
+++ Template/Graph/stronglyCC.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef vector<vector<int> > vvi;
+typedef vector<string> vs;
+typedef complex<double> cd;
+#define rep(i,l,r) for(int i=l;i<=r;++i)
+#define per(i,r,l) for(int i=r;i>=l;--i)
+#define rep0(i,l,r) for(int i=l;i<r;++i)
+#define forn(i,n) for(int i=0;i<n;++i)
+#define all(a) (a).begin(), (a).end()
+#define allr(a) (a).rbegin(), (a).rend()
+#define foreach(a) for(auto it = a.begin(); it != a.end(); it++)
+#define mem(a,b) memset(a, (b), sizeof(a))
+
+const int maxn = 1e5 + 5;
+const ll MOD = 1e9 + 7; // 998244353
+const ll INF = 1e9;
+const int LOG = 26;
+const char min_char = 'a';
+const double EPS = 1e-9;
+const double PI = 3.14159265358979323846;
+
+int n;
+vector<vector<int>> adj, adj_rev;
+vector<bool> used;
+vector<int> order, component;
+
+void dfs1(int v) {
+    used[v] = true;
+
+    for (auto u : adj[v])
+        if (!used[u])
+            dfs1(u);
+
+    order.push_back(v);
+}
+
+void dfs2(int v) {
+    used[v] = true;
+    component.push_back(v);
+
+    for (auto u : adj_rev[v])
+        if (!used[u])
+            dfs2(u);
+}
+
+void stronglyConnectedCC(){
+    used.assign(n, false);
+
+    for (int i = 0; i < n; i++)
+        if (!used[i])
+            dfs1(i);
+
+    used.assign(n, false);
+    reverse(order.begin(), order.end());
+
+    for (auto v : order){
+        if (!used[v]) {
+            dfs2 (v);
+            // processing next component
+            component.clear();
+        }
+    }
+}
+
+vector<int> roots(maxn, 0);
+vector<int> root_nodes;
+vector<vector<int>> adj_scc(n);
+
+void condensedGraph(){
+    used.assign(n, false);
+
+    for (int i = 0; i < n; i++)
+        if (!used[i])
+            dfs1(i);
+
+    used.assign(n, false);
+    reverse(order.begin(), order.end());
+
+    for (auto v : order){
+        if (!used[v]) {
+            dfs2(v);
+
+            int root = component.front();
+            for (auto u : component) roots[u] = root;
+            root_nodes.push_back(root);
+
+            component.clear();
+        }
+    }
+
+    for (int v = 0; v < n; v++){
+        for (auto u : adj[v]) {
+            int root_v = roots[v], root_u = roots[u];
+            if (root_u != root_v) 
+                adj_scc[root_v].push_back(root_u);
+        }
+    }
+
+}
+void solve(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    auto start = high_resolution_clock::now();
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cerr << "time: " << duration.count() << "ms\n";
+}
