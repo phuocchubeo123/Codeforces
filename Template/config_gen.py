35c4e4e266a9d1da43c1996e5bafd66fbd8a5779
phuocchubeo123 <phuoc002@e.ntu.edu.sg>
2023-02-01 22:27:15 +0800

    div2 848

--- Template/config_gen.py
+++ Template/config_gen.py
@@ -1,3 +1,3 @@
 [CONTEST]
-folder_name = ..//Contests//Misc//TypeDB2023
-prob_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+folder_name = ..//Contests//Div2//848
+prob_list = ['a', 'b', 'c', 'd', 'e', 'f']
