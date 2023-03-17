306bd17b339d0338ca6626625cd86386b052a201
phuocchubeo123 <phuoc002@e.ntu.edu.sg>
2023-03-17 23:28:15 +0800

    icpc train

--- Template/config_gen.py
+++ Template/config_gen.py
@@ -1,3 +1,3 @@
 [CONTEST]
-folder_name = ..//Contests//Div1//850
-prob_list = ['a', 'b', 'c', 'd', 'e', 'f']
+folder_name = ..//ICPCTrain//22231
+prob_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
